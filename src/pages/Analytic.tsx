765942966699ca722f61d9b37e275402d6490130
Faizahel Joasa Ariesta <faizaheljoasaariesta@gmail.com>
2025-12-05 15:08:06 +0800

    @Faizahel actived page analytic

--- src/pages/Analytic.tsx
+++ src/pages/Analytic.tsx
@@ -23,10 +23,10 @@ export default function Analytic() {
         <div className="flex flex-1 flex-col">
           <div className="@container/main flex flex-1 flex-col gap-2">
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-              {/* <div className="px-4 lg:px-6">
+              <div className="px-4 lg:px-6">
                 <AAChart />
               </div>
-              <AnalyticTable /> */}
+              <AnalyticTable />
             </div>
           </div>
         </div>
